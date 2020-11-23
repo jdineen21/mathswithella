529ac23758a75061bc3e6c86fee213d89e6ce8cb
Joe Dineen <jdineen21@gmail.com>
2020-11-23 17:35:52 +0000

    base templates and css

--- mathswithella/settings/base.py
+++ mathswithella/settings/base.py
@@ -29,6 +29,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
